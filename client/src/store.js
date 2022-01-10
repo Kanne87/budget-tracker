80d5c7ba63d9be959bd58737c2b7308a812af5ee
Kai Lohmann <mail@kl-testground.de>
2022-01-10 19:39:09 +0100

    oauhf

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
@@ -9,7 +10,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
+  composeWithDevTools(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
